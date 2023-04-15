15c02df90aa419e9b678bac56f5391fb62052451
Tristan Copley <89219797+TristanCopley@users.noreply.github.com>
2023-04-15 11:25:56 -0700

    Changing around website

---
+++ src/routes/Landing.svelte
@@ -0,0 +1,11 @@
+<div class='flex w-screen h-screen bg-primary justify-center gap-8'>
+
+	<div class='w-96 h-72 bg-secondary self-center'>
+		Text
+	</div>
+
+	<div class='w-96 h-72 bg-secondary self-center'>
+		Text
+	</div>
+
+</div>
