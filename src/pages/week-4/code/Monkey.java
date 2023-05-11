f850971c9330165df09fb125902d44b1ed8cc42e
SlimeyTurtles <avinhahuynh@gmail.com>
2023-05-11 10:32:57 -0700

    week 4

--- src/pages/week-4/code/Monkey.java
+++ src/pages/week-4/code/Monkey.java
@@ -1,16 +1,3 @@
-# Iteration with 2D Array
-> Focus is on iteration.  This example uses 2D array with simple ASCII art.  The idea of this example was to incorporate ASCII art with a nursery rhyme.
-
-- title: Iteration with 2D Array
-- toc: true
-- image: /images/boolean.png
-- permalink: /unit/4
-- categories: [3.E, C4.7]
-- type: ap
-- week: 4
-
-
-```java
 /*
  * Creator: Nighthawk Coding Society
  * Mini Lab Name: Hello Series,featuring Monkey Jumpers
@@ -125,108 +112,10 @@ class MonkeyLoop {
     }
 
 }
-MonkeyLoop.main(null);
-```
 
-    
-    Monkey Jumpers Poem in Java Loopy
-    5 little monkeys jumping on the bed...
-    ʕง ͠° ͟ل͜ ͡°)ʔ  
-      \_⏄_/   
-      --0--    
-      ⎛   ⎞    
-    
-     ʕ༼ ◕_◕ ༽ʔ 
-      \_⎏_/   
-      ++1++   
-       ⌋ ⌊    
-    
-     ʕ(▀ ⍡ ▀)ʔ 
-      \_⎐_/  
-      <-2->   
-      〈  〉  
-    
-    ʕ ͡° ͜ʖ ° ͡ʔ 
-      \_⍾_/   
-      ==3==   
-      _/ \_   
-    
-      (◕‿◕✿)  
-      \_⍾_/  
-      ==4==   
-      _/ \_  
-    
-    4 little monkeys jumping on the bed...
-    ʕง ͠° ͟ل͜ ͡°)ʔ  
-      \_⏄_/   
-      --0--    
-      ⎛   ⎞    
-    
-     ʕ༼ ◕_◕ ༽ʔ 
-      \_⎏_/   
-      ++1++   
-       ⌋ ⌊    
-    
-     ʕ(▀ ⍡ ▀)ʔ 
-      \_⎐_/  
-      <-2->   
-      〈  〉  
-    
-    ʕ ͡° ͜ʖ ° ͡ʔ 
-      \_⍾_/   
-      ==3==   
-      _/ \_   
-    
-    3 little monkeys jumping on the bed...
-    ʕง ͠° ͟ل͜ ͡°)ʔ  
-      \_⏄_/   
-      --0--    
-      ⎛   ⎞    
-    
-     ʕ༼ ◕_◕ ༽ʔ 
-      \_⎏_/   
-      ++1++   
-       ⌋ ⌊    
-    
-     ʕ(▀ ⍡ ▀)ʔ 
-      \_⎐_/  
-      <-2->   
-      〈  〉  
-    
-    2 little monkeys jumping on the bed...
-    ʕง ͠° ͟ل͜ ͡°)ʔ  
-      \_⏄_/   
-      --0--    
-      ⎛   ⎞    
-    
-     ʕ༼ ◕_◕ ༽ʔ 
-      \_⎏_/   
-      ++1++   
-       ⌋ ⌊    
-    
-    1 little monkeys jumping on the bed...
-    ʕง ͠° ͟ل͜ ͡°)ʔ  
-      \_⏄_/   
-      --0--    
-      ⎛   ⎞    
-    
-    No more monkeys jumping on the bed
-    0000000000000000000000000000000000
-                 THE END              
-
-
-## Hacks (Mini-lab)
-> Build you own Jupyter Notebook.  Feel free to use any ASCII art of your choice, there are some much better ones [here](https://www.asciiart.eu/animals/monkeys).  My little guys were made up out of my head while looking at unicode characters.
-
-- Print monkeys horizontally versus vertically.
-- Build more or entire rhyme for the "Monkey Jumpers" countdown poem
-- Add names or other properties to the monkeys
- 
-> In you notebook, illustrate or answer some of these questions.
-- Is this program in more of an Imperative Programming Style or OOP style? Explain.
-    - Observe variable assignments.
-    - Is each Monkey an object?
-    - Build an where the monkey is an object versus two-dimensional array.  This would be leading into Unit 5 requirements.
-- Study loops and zero based counting
-    - Study two-dimensional (2D) array references
-    - Explain different way you can access a 2D array
+// Judge0 Activation (pretend it's not here)
+class Main {
+    public static void main(String[] args) {
+        MonkeyLoop.main(null);
+    }
+}
