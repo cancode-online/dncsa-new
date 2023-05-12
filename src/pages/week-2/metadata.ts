eef36bb74c5498117227ac01b55cc9800933136a
SlimeyTurtles <avinhahuynh@gmail.com>
2023-05-12 10:47:46 -0700

    changed order of weeks

--- src/pages/week-2/metadata.ts
+++ src/pages/week-2/metadata.ts
@@ -1,4 +1,4 @@
-export const date = '2022-08-9';
+export const date = '2022-08-22';
 export const title = 'Week 2';
 export const type = 'week';
 export const order = ['anthony_and_sahil', 'using_java_objects', 'console_games'];
