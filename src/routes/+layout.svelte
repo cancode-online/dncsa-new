226ec13514d56d1b10a88167cd7d245d6a5e74e2
akhil <akhil@nandhakumar.net>
2023-05-15 21:30:45 -0700

    working popup for infor verification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,64 @@
 <script lang="ts">
-	import '../app.css';
-	import NavigationBar from '$components/navbar/NavigationBar.svelte';
-  	import Loading from '$components/Loading.svelte';
-	import { authenticated } from '$firebase';
-
-	import { fly, fade } from 'svelte/transition';
-  	import Dashboard from './Dashboard.svelte';
-	import AuthForm from '\$/lib/components/AuthForm.svelte';
-
+	import "../app.css";
+	import NavigationBar from "$components/navbar/NavigationBar.svelte";
+	import Loading from "$components/Loading.svelte";
+	import { authenticated } from "$firebase";
+	import { fly } from "svelte/transition";
+	import Dashboard from "./Dashboard.svelte";
+	import AuthForm from "$/lib/components/AuthForm.svelte";
+	import VerifyPopup from "$/lib/components/VerifyPopup.svelte";
+	import { fade } from "svelte/transition";
+  
 	let _authenticated = false;
 	let _loading = true;
-
+  
 	authenticated.subscribe((state) => {
-		_authenticated = state || false;
+	  _authenticated = state || false;
 	});
-
+  
 	setTimeout(() => {
-		_loading = false;
+	  _loading = false;
 	}, 500); // ms
 
-	// Could possibly use $authentication but I'm not sure if that is optimal or not
-</script>
 
-<div class="transition-container w-full h-full">
+  </script>
+  
+  <div class="transition-container w-full h-full">
 	{#if _loading}
-		<Loading />
+	<Loading />
 	{:else}
-
-		<div
-			in:fly={{
-				x: 0,
-				y: -100,
-				duration: 1000,
-				delay: 100
-			}}
-		>
-			<NavigationBar />
-		</div>
-
-		{#if _authenticated}
-			<Dashboard>
-				<slot />
-			</Dashboard>
-		{:else}
-			<div
-				class="bg-blue-500 w-full h-screen flex justify-center"
-			>
-				<AuthForm/>
-			</div>
-		{/if}
+	<div
+	  in:fly={{
+		x: 0,
+		y: -100,
+		duration: 1000,
+		delay: 100,
+	  }}
+	>
+	  <NavigationBar />
+	</div>
+	{#if _authenticated}
+	<Dashboard>
+	  <slot />
+	</Dashboard>
+	{:else}
+	<div class="bg-blue-500 w-full h-screen flex justify-center">
+	  <AuthForm />
+	</div>
 	{/if}
-</div>
+	{/if}
+
+	{#if _authenticated}
+		<div
+		in:fly={{
+			x: 0,
+			y: 100,
+			duration: 300,
+			delay: 1000,
+		}}
+		>
+		<VerifyPopup/>
+	  </div>
+	{/if}
+	
+  </div>
