5eda0fdc7a144fffa7e10ff749c3e6eebab19e5f
Rebecca-123 <rebecca.notw@gmail.com>
2023-05-16 10:36:40 -0700

    fix prop

--- src/routes/page/[page]/+layout.svelte
+++ src/routes/page/[page]/+layout.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-    import MarkdownPage from '$/lib/components/MarkdownPage.svelte';
-    import Tabs from '$/lib/components/Tabs.svelte';
-    import { page } from '$app/stores';
+	import MarkdownPage from '$/lib/components/MarkdownPage.svelte';
+	import Tabs from '$/lib/components/Tabs.svelte';
+	import { page } from '$app/stores';
 
-    let currentPage = '/';
-    $: currentPage = $page.params.page;
-
+	let currentPage = '/';
+	$: currentPage = $page.params.page;
 </script>
 
-<div class='flex flex-col bg-blue-500 min-h-[20rem] w-full'>
+<div class="flex flex-col bg-blue-500 min-h-[20rem] w-full">
+	<div class="w-full">
+		<Tabs />
+	</div>
 
-    <div class='w-full'>
-        <Tabs />
-    </div>
+	<div class="bg-lime-500 min-h-[40rem]">
+		{#key currentPage}
+			<MarkdownPage webpage={currentPage} />
+		{/key}
+	</div>
 
-    <div class='bg-lime-500 min-h-[40rem]'>
-        {#key currentPage}
-            <MarkdownPage webpage={"unit-5"} />
-        {/key}
-    </div>
-
-    <div class='bg-purple-500 min-h-[40rem] mt-4'>
-        {#key currentPage}
-            Extra
-            <slot />
-        {/key}
-    </div>
-
+	<div class="bg-purple-500 min-h-[40rem] mt-4">
+		{#key currentPage}
+			Extra
+			<slot />
+		{/key}
+	</div>
 </div>
